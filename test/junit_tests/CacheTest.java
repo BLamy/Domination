d3fb68c479981d919e69784ae4e104d0f62b8acc
zjd2035 <zjd2035@gmail.com>
2016-10-27 14:37:04 -0400

    Added a first junit test in /test/junit_tests/CacheTest. Havent written a test yet but you can at least add more tests here.

---
+++ test/junit_tests/CacheTest.java
@@ -0,0 +1,21 @@
+package junit_tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by zack on 10/27/16.
+ */
+public class CacheTest extends TestCase {
+    public void testPut() throws Exception {
+
+    }
+
+    public void testGet() throws Exception {
+
+    }
+
+    public void testContainsKey() throws Exception {
+
+    }
+
+}
