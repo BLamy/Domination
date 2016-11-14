114323e3429a9105e41164a730f5ab81d67707d7
zjd2035 <zjd2035@gmail.com>
2016-11-14 15:02:57 -0500

    Trying to fix build for netbeans and added some basic unit tests

--- test/junit/net/yura/cache/CacheTest.java
+++ test/junit/net/yura/cache/CacheTest.java
@@ -1,8 +1,31 @@
-package junit.net.yura.cache;
+package net.yura.cache;
+
+import net.yura.cache.Cache;
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.io.File;
 
 /**
  * Created by zack on 10/31/16.
  */
-public class CacheTest {
+public class CacheTest extends TestCase {
+    
+    public CacheTest(String testName) {
+        super(testName);
+    }
 
+    public void testSmoke() {
+        assert(true);
+    }
+    
+    public void testCacheConstructor() {
+        Cache test1Cache = new Cache("test");
+        Cache test2Cache = new Cache("test");
+        Cache test3Cache = new Cache("test");
+        String cacheFile = System.getProperty("java.io.tmpdir");
+        cacheFile = cacheFile.concat("test.cache");
+        File varTmpDir = new File(cacheFile);
+        boolean exists = varTmpDir.exists();
+        assertEquals(true, exists);
+    }
 }
---
+++ test/junit/net/yura/domination/engine/guishared/GuiSharedTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.yura.domination.engine.guishared;
+
+import net.yura.domination.engine.guishared.MapMouseListener;
+import net.yura.domination.engine.guishared.PicturePanel;
+import net.yura.domination.engine.Risk;
+import net.yura.domination.engine.core.RiskGame;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author zack
+ */
+public class GuiSharedTest extends TestCase {
+    
+    public GuiSharedTest(String testName) {
+        super(testName);
+    }
+
+    public void testSmoke() {
+        assert(true);
+    }
+    
+//    public void testConstructor() throws Exception {
+//        Risk myrisk = new Risk();
+//        PicturePanel pp = new PicturePanel(myrisk);
+//        MapMouseListener mml = new MapMouseListener(myrisk, pp);
+//    }
+//    
+}
